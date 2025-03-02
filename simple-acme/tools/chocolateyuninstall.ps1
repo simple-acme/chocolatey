5c0ee082d63764c527ddb457012a7f275e122048
Wouter Tinus <win.acme.simple@gmail.com>
2025-03-02 06:49:35 +0100

    Release v2.3.1.9

---
+++ simple-acme/tools/chocolateyuninstall.ps1
@@ -0,0 +1,14 @@
+﻿$ErrorActionPreference = 'Stop';
+$destination           = Join-Path $(Get-ToolsLocation) 'simple-acme'
+$exepath               = (Join-Path $destination 'wacs.exe')
+$tag                   = "v2.323."
+
+$packageArgs = @{
+    FilePath   = "simple-acme.$tag.win-x86.pluggable.zip"
+    FilePath64 = "simple-acme.$tag.win-x64.pluggable.zip"
+}
+
+Uninstall-ChocolateyZipPackage -PackageName $env:ChocolateyPackageName -ZipFileName $packageArgs.FilePath
+Uninstall-ChocolateyZipPackage -PackageName $env:ChocolateyPackageName -ZipFileName $packageArgs.FilePath64
+
+Uninstall-BinFile -Name 'wacs' -Path $exepath
