0bdd1882c06167023883551f7df0b2b4f67b56b4
Wouter Tinus <win.acme.simple@gmail.com>
2025-03-05 12:46:28 +0100

    Release v2.3.1.9

--- simple-acme/tools/chocolateyinstall.ps1
+++ simple-acme/tools/chocolateyinstall.ps1
@@ -1,5 +1,6 @@
 $ErrorActionPreference = 'Stop'
 $toolsDir              = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+$parentDir             = (Get-Item $toolsDir).Parent.FullName
 $destination           = Join-Path $(Get-ToolsLocation) 'simple-acme'
 $exepath               = (Join-Path $destination 'wacs.exe')
 $build                 = "2.3.1.1894"
@@ -7,10 +8,10 @@ $build                 = "2.3.1.1894"
 $packageArgs = @{
     packageName    = "$env:chocolateyPackageName"
     destination    = $destination
-    FileFullPath   = Join-Path $toolsDir "\bin\simple-acme.v$build.win-x86.pluggable.zip"
-    FileFullPath64 = Join-Path $toolsDir "\bin\simple-acme.v$build.win-x64.pluggable.zip"
+    FileFullPath   = Join-Path $parentDir "\bin\simple-acme.v$build.win-x86.pluggable.zip"
+    FileFullPath64 = Join-Path $parentDir "\bin\simple-acme.v$build.win-x64.pluggable.zip"
 }
 
 Get-ChocolateyUnzip @packageArgs
 Install-BinFile -Name 'wacs' -Path $exepath
-Remove-Item -ea 0 -force -path $toolsDir\*.zip
+Remove-Item -ea 0 -force -path $parentDir\*.zip
