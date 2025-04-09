2c9e13d61d6a006492a4991527829efc977b3184
Wouter Tinus <win.acme.simple@gmail.com>
2025-04-09 21:56:08 +0200

    Release v2.3.1.9

--- simple-acme-plugin-template/tools/chocolateyinstall.ps1
+++ simple-acme-plugin-template/tools/chocolateyinstall.ps1
@@ -7,7 +7,7 @@ $artifact              = ""
 $packageArgs = @{
     packageName    = "$env:chocolateyPackageName"
     destination    = $destination
-    FileFullPath   = Join-Path $toolsDir "\bin\$artifact"
+    FileFullPath   = Join-Path $parentDir "\bin\$artifact"
 }
 
 Get-ChocolateyUnzip @packageArgs
